853210393398d51bb9e829e3df7b6024d1c33ce4
Abdillahi485 <daakircadde143@gmail.com>
2023-12-03 14:41:31 +0300

    finished count charecters

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Good Luck 💪🏾
+function count(letter,word){
+    let noOfletters=0
+    for(let i=0; i<word.length;i++){
+        
+        if(letter === word[i]){
+
+            noOfletters ++
+            
+        }
+    }
+   
+    return noOfletters
+}
+let countResult1=count("a", "fikrcamp")
+let countResult2=count("c", "bootcamp")
+let countResult3=count("o", "fikrcamp coding bootcamp")
+console.log(countResult1)
+console.log(countResult2)
+console.log(countResult3)
